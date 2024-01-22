66ffa333511a0528ef0ac3997fbee313f591ec4e
Fer457 <fernandonevothernandez@gmail.com>
2024-01-22 11:44:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime-types");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hola</h1>");
+});
+
+app.get("/descargar-archivo", (req, res) => {
+  const archivoPath = path.join(__dirname, "archivos", "ejemplo.txt");
+
+  if (!fs.existsSync(archivoPath)) {
+    return res.status(404).send("Archivo no encontrado");
+  }
+
+  const mimeType = mime.lookup(archivoPath);
+
+  res.setHeader("Content-disposition", "attachment; filename=ejemplo.txt");
+  res.setHeader("Content-type", mimeType);
+
+  const archivoStream = fs.createReadStream(archivoPath);
+  archivoStream.pipe(res);
+});
+
+// Iniciar el servidor
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
+});
